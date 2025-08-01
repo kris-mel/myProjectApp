81881d5d2992c55dbe18dc2f22c167ce8e2868e0
Kristina <7raiwen@gmail.com>
2025-08-01 18:09:23 +0300

    changed module.css for Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
+import styles from './Navbar.module.css';
+
 const Navbar = () => {
     return (
-        <div>
+        <div className={styles.layout}>
             <div>
                 <a href='#s'>My profile</a>
             </div>
