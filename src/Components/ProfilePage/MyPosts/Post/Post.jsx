4b3a09119b91e489abb95c6763153999f21d4088
Kristina <7raiwen@gmail.com>
2025-08-19 20:10:19 +0300

    Added comments

--- src/Components/ProfilePage/MyPosts/Post/Post.jsx
+++ src/Components/ProfilePage/MyPosts/Post/Post.jsx
@@ -6,11 +6,14 @@ const Post = (props) => {
     return (
         <div className={styles.item}>
             <div>
+                 {/* Аватарка автора поста */}
                 <img src='https://i.pinimg.com/736x/23/98/d7/2398d75d71c3219583ae91457709d3a0.jpg' />
             </div>
+            {/* Текст поста */}
             {props.message}
             <div>
-                <LikeButton initialCount={15} />
+                {/* Лайки */}
+                <LikeButton initialCount={props.initialCount} />
             </div>
         </div>
     );
